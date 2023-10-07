67a27810ea6de4edfd7339291a05dd5ecb38d0b8
Calvin Haynes <chx1006488386@163.com>
2023-10-07 13:57:28 +0800

    发现忘了debug exerciese2,现在补上了

--- lab2/DebugExercise/DebugExercise2.java
+++ lab2/DebugExercise/DebugExercise2.java
@@ -7,13 +7,11 @@ package DebugExercise;
 public class DebugExercise2 {
     /** Returns the max of a and b. Do not step into this function. */
     public static int max(int a, int b) {
-        int w = (b - a) >> 31;
-        /* If you're stepping into this function, click the
-           step out button because you're not going to learn anything. */
-        int z = ~(b - a) >> 31;
-
-        int max = b & w | a & z;
-        return max;
+        if (a >= b) {
+            return a;
+        } else {
+            return b;
+        }
     }
 
 
@@ -55,11 +53,9 @@ public class DebugExercise2 {
 
     /** Returns the sum of all elements in x. */
     public static int arraySum(int[] x) {
-        int i = 0;
         int sum = 0;
-        while (i < x.length) {
-            sum = sum + add(sum, x[i]);
-            i = i + 1;
+        for (int i = 0; i < x.length; i++) {
+            sum = sum + x[i];
         }
         return sum;
     }
