759bad6e3291f3d7e683f688c4f66211f533f6e3
Calvin Haynes <chx1006488386@163.com>
2023-10-09 16:00:32 +0800

    test

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -18,13 +18,13 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-       if (n == 1){
-           return n;
-       } else if (n % 2 == 0) {
-           return n / 2;
-       } else {
-           return 3 * n + 1;
-       }
+        if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
+            return 3 * n + 1;
+        } else {
+            return n * 2;
+        }
     }
 
     public static void main(String[] args) {
