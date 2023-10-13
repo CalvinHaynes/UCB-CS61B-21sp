0664d454f01f29f41b8c7cb4a1b1d024a264145e
Calvin Haynes <chx1006488386@163.com>
2023-10-13 20:25:44 +0800

    修改resize方法的Alist上传

--- lab3/timingtest/AList.java
+++ lab3/timingtest/AList.java
@@ -18,6 +18,8 @@ public class AList<Item> {
     private Item[] items;
     private int size;
 
+    private static double RFACTOR = 1.01;
+
     /** Creates an empty list. */
     public AList() {
         items = (Item[]) new Object[100];
@@ -34,7 +36,7 @@ public class AList<Item> {
     /** Inserts X into the back of the list. */
     public void addLast(Item x) {
         if (size == items.length) {
-            resize(size + 1);
+            resize((int) (size * RFACTOR));
         }
 
         items[size] = x;
