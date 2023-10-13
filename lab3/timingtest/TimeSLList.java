fba112c5dad068bbe7f31fe93218a0754aa529e1
Calvin Haynes <chx1006488386@163.com>
2023-10-13 20:54:44 +0800

    TimeSLList.java做完

--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -1,5 +1,7 @@
 package timingtest;
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Stopwatch;
+import org.checkerframework.checker.units.qual.A;
 
 /**
  * Created by hug.
@@ -23,6 +25,29 @@ public class TimeSLList {
 
     public static void timeGetLast() {
         // TODO: YOUR CODE HERE
+        SLList<Integer> testSLList = new SLList<>();
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+
+        for (int i = 0; i < 8; i++) {
+            int pow2 = 1 << i; //求2的i次方
+            Ns.addLast(1000 * pow2);
+            opCounts.addLast(10000);
+        }
+
+        for (int i = 0; i < Ns.size(); i++) {
+            for (int j = 0; j < Ns.get(i); j++) {
+                testSLList.addLast(0);
+            }
+            Stopwatch sw = new Stopwatch();
+            for (int k = 0; k < opCounts.get(i); k++) {
+                testSLList.getLast();
+            }
+            double timeInSeconds = sw.elapsedTime();
+            times.addLast(timeInSeconds);
+        }
+        printTimingTable(Ns,times,opCounts);
     }
 
 }
